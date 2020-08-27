192f867d8bbc73a784dfcdce8624977004464d26
leetaeyun <taeyun90243@snu.ac.kr>
2020-08-27 18:27:45 +0900

    linear regression added, 202008271827

--- nucleo_small/lib/Stall_Load_detection/Stall_Load_Dectection.cpp
+++ nucleo_small/lib/Stall_Load_detection/Stall_Load_Dectection.cpp
@@ -1,143 +1,75 @@
 #include "Stall_Load_Detection.h"
 
+#define CURRENT_LEN 500
+#define X_SUM 249500 //0~998 SUM OF MULTIPLES OF 2
+#define X_SQSUM 166167000
 
+double A, b, A_m, b_m;
 
-double A,b, A_m,b_m;
 double Read_LPF_current(){
-  static int first_=1;
-  static int prev_lpf=0;
+  static int first_ = 1;
+  static double prev_lpf = 0;
   int data = analogRead(READ_CURRENT);
-  if(first_ ==1){
-    first_ =0;
-    return prev_lpf = LPF(data,0.99,prev_lpf,first_); 
+  if (first_ == 1){
+    prev_lpf = LPF(data, 0.99, prev_lpf, first_);
+    first_ = 0;
+    return prev_lpf;
   }
   else{
-    return prev_lpf = LPF(data,0.99,prev_lpf,first_);
+    return prev_lpf = LPF(data, 0.99, prev_lpf, first_);
   }
 }
 
-void Get_Linear_Regression(AccelStepper& stepper){
-    double mic_current[200];
-    double _current[300];
-
-    int speed = 0;
-    int last_time = millis();
-    
-    while(speed <400){
-        digitalWrite(SET_MS1,HIGH);
-        stepper.setSpeed(speed*2);
-        stepper.runSpeed();
-        Read_LPF_current();
-        if(millis()-last_time >10){
-
-            mic_current[speed/2] = Read_LPF_current();
-            speed = speed+2;
-            last_time = millis();
-        }
-    }
-    while(speed<1000){
-        digitalWrite(SET_MS1,LOW);
-        stepper.setSpeed(speed);
-        stepper.runSpeed();
-        Read_LPF_current();
-        if(millis()-last_time >10){
-            _current[speed/2-200] = Read_LPF_current();
-            speed = speed+2;
-            last_time = millis();
-        }
-    }
-    
-    
-    /*    calculate A_m,b_m      */
-    double X_m = 195;
-    double Y_m =0;
-    for(int i=0; i<200 ; i++){
-      Y_m = Y_m + mic_current[i]/40.0;
-    }
-    double numerator=0;
-    for(int i=0; i<200 ; i++){
-      numerator = numerator + (10*i-X_m)*(mic_current[i]-Y_m);
-    }
-    double denumerator=0;
-    for(int i=0; i<200 ; i++){
-      denumerator = denumerator + (10*i-X_m)*(10*i-X_m);
-    }
-    A_m = numerator/denumerator;
-    b_m = Y_m - A_m*X_m;
-    //Let's calculate A,b
-
-    double X_ = 695;
-    double Y_ =0;
-    for(int i=0; i<300 ; i++){
-      Y_ = Y_ + _current[i]/60.0;
-    }
-    numerator=0;
-    for(int i=0; i<300 ; i++){
-      numerator = numerator + (10*(i+40)-X_)*(_current[i]-Y_);
-    }
-    denumerator=0;
-    for(int i=0; i<300 ; i++){
-      denumerator = denumerator + (10*(i+40)-X_)*(10*(i+40)-X_);
-    }
-    A = numerator/denumerator;
-    b = Y_ - A*X_;
-
-    for(int i=0; i<200 ; i++){
-      //Serial.println(mic_current[i]);
-    }
-      Serial.println();
-    for(int i=0; i<300 ; i++){
-      Serial.println(_current[i]);
-    }
-   
-
-}
-
-double LPF(int data,double alpha,double prev_lpf,int first_time){
-  if(first_time ==1){
+double LPF(int data, double alpha, double prev_lpf, int first_time)
+{
+  if (first_time == 1)
+  {
     return data;
   }
-  else{
-    return alpha*prev_lpf + (1-alpha)*data;
+  else
+  {
+    return alpha * prev_lpf + (1 - alpha) * data;
   }
 }
 
-int section_sum(){
-  
-  int sum=0;
-  for( int i=0; i<SAMPLES; i++){
-    ;
-    sum = sum+analogRead(READ_CURRENT);
+int section_sum()
+{
+
+  int sum = 0;
+  for (int i = 0; i < SAMPLES; i++){
+    sum = sum + analogRead(READ_CURRENT);
   }
-    Serial.println(sum);
+  Serial.println(sum);
 }
 
-
-void Set_motor(AccelStepper stepper,double speed, int dir){
-    if(speed <400 ){
-        digitalWrite(SET_MS1,HIGH);
-        stepper.setSpeed(speed*2*dir);
-    }
-    else{
-        digitalWrite(SET_MS1,LOW);
-        stepper.setSpeed(speed*dir);
-    }
+void Set_motor(AccelStepper stepper, double speed, int dir){
+  if (speed < 400){
+    digitalWrite(SET_MS1, HIGH);
+    stepper.setSpeed(speed * 2 * dir);
+  }
+  else
+  {
+    digitalWrite(SET_MS1, LOW);
+    stepper.setSpeed(speed * dir);
+  }
 }
 
+double get_speed()
+{
+  static unsigned long last_time = micros();
+  unsigned long temp = micros() - last_time;
 
-double get_speed(){
-    static unsigned long last_time = micros();
-    unsigned long  temp = micros()-last_time;
-
-    last_time =  temp+ last_time;
-    return (double)1/temp*1000000;
+  last_time = temp + last_time;
+  return (double)1 / temp * 1000000;
 }
 
-double calculate_current(int speed){
-    if(speed<400){
-        return speed*A_m +b_m;
-    }
-  return speed*A+b;
+double calculate_current(int speed)
+{
+  if (speed < 400)
+  {
+    return speed * A_m + b_m;
+  }
+  return speed * A + b;
 }
 
 /*
@@ -169,3 +101,82 @@ void moving_sum(int group){
   }
 }
 */
+
+void Get_Linear_Regression2(AccelStepper &stepper)
+{
+  double _current[CURRENT_LEN];
+
+  int speed = 0;
+  int last_time = millis();
+
+  while (speed < 1000){
+    digitalWrite(SET_MS1, LOW);
+    stepper.setSpeed(speed);
+    stepper.runSpeed();
+    Read_LPF_current();
+    if (millis() - last_time > 10){
+      stepper.runSpeed();
+      _current[speed / 2] = Read_LPF_current();
+      speed = speed + 2;
+      last_time = millis();
+      stepper.runSpeed();
+    }
+  }
+  
+  double X_ = X_SUM;
+  double X_sq =X_SQSUM;
+  double Y_ = 0;
+  double XY_ = 0;
+  for (int i = 0; i < CURRENT_LEN; i++){
+    Y_ = Y_ + _current[i];
+    XY_ = XY_ + _current[i]*(2*i);
+  }
+  
+  double denum = CURRENT_LEN*X_sq-(X_*X_);
+  b = (Y_*X_sq - X_*XY_)/denum;
+  A=(500*XY_- X_*Y_)/denum;
+
+
+  for (int i = 0; i < CURRENT_LEN; i++)
+  {
+    Serial.println(_current[i]);
+  }
+  Serial.println(b,10);//second parameter is precision
+  Serial.println(A,10);
+}
+
+void Get_Linear_Regression3(AccelStepper &stepper)
+{
+  double X_ = X_SUM;
+  double X_sq =X_SQSUM;
+  double Y_ = 0;
+  double XY_ = 0;
+
+  int speed = 0;
+  int last_time = millis();
+  double temp;
+  while (speed < 1000){
+    digitalWrite(SET_MS1, LOW);
+    stepper.setSpeed(speed);
+    stepper.runSpeed();
+    Read_LPF_current();
+    if (millis() - last_time > 10){
+      stepper.runSpeed();
+      temp = Read_LPF_current();
+      Y_ = Y_+temp;
+      XY_ = XY_ + temp*speed;
+      speed = speed + 2;
+      last_time = millis();
+      stepper.runSpeed();
+    }
+  }
+
+  
+  double denum = CURRENT_LEN*X_sq-(X_*X_);
+  b = (Y_*X_sq - X_*XY_)/denum;
+  A=(CURRENT_LEN*XY_- X_*Y_)/denum;
+
+
+  Serial.println(b,10);//second parameter is precision
+  Serial.println(A,10);
+}
--- nucleo_small/src/main.cpp
+++ nucleo_small/src/main.cpp
@@ -1,31 +1,59 @@
-#include <Arduino.h>
 #include "Stall_Load_Detection.h"
-
 
 AccelStepper stepper1(AccelStepper::DRIVER, SET_SPEED, SET_DIR);
 
-
 int data;
-int speed=0;
-int last_time;
-int dir=0;
-void setup() {
+int speed = 0;
+unsigned long last_time;
+int dir = 0;
+void setup()
+{
   Serial.begin(9600);
   stepper1.moveTo(400);
-  stepper1.setAcceleration(6400);
+  stepper1.setAcceleration(200);
   stepper1.setMinPulseWidth(20);
-  stepper1.setMaxSpeed(1000);
-  stepper1.setSpeed(60);
-  speed=0;
+  stepper1.setMaxSpeed(2000);
+  //stepper1.setSpeed(0);
+
+  pinMode(SET_MS1, OUTPUT);
+  digitalWrite(SET_MS1,LOW);
   last_time = millis();
+  
   dir=0;
-  Get_Linear_Regression(stepper1);
+  //Get_Linear_Regression3(stepper1);
+  
 }
 
-
-
-
-
-void loop() {
+void loop()
+{
+   stepper1.runSpeed();
+  
+  /*
+  if (millis() - last_time > 1)
+  {
+    stepper1.runSpeed();
+    if (dir == 0)
+    {
+      stepper1.runSpeed();
+      speed = speed + 2;
+      if (speed == 1000)
+        dir = 1;
+      stepper1.setSpeed(speed);
+      stepper1.runSpeed();
+    }
+    else
+    {
+      stepper1.runSpeed();
+      speed = speed - 2;
+      if (speed == 0)
+        dir = 0;
+      stepper1.setSpeed(speed);
+      stepper1.runSpeed();
+    }
+    last_time = millis();
+  } //Serial.println(speed); //Serial.println(dir); stepper1.runSpeed(); //stepper1.runSpeed()
   stepper1.runSpeed();
+  //Serial.println(speed);
+*/
+  
 }
