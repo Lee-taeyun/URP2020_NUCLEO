4a6d1ec287278fa4ff85f0e36a45bedf7d9a10c6
jaeyeon park <parkjani2@gmail.com>
2020-08-28 14:22:16 +0900

    Add interrupt routines to pass the motor command signal from external mcu

--- IntelliMotor/src/main.cpp
+++ IntelliMotor/src/main.cpp
@@ -1,8 +1,6 @@
 #include <mbed.h>
 #include "step_listener.h"
 int main() {
-  StepListener intelliDriver();
-  // put your setup code here, to run once:
 
   while(1) {
     // put your main code here, to run repeatedly:
