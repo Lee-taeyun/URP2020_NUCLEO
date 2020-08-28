75927ec9f7ab492d36ac6adf7cd5f2fda2b7cd92
jaeyeon park <parkjani2@gmail.com>
2020-08-28 13:04:23 +0900

    add Pin definitions

---
+++ IntelliMotor/src/main.cpp
@@ -0,0 +1,10 @@
+#include <mbed.h>
+#include "step_listener.h"
+int main() {
+  StepListener intelliDriver();
+  // put your setup code here, to run once:
+
+  while(1) {
+    // put your main code here, to run repeatedly:
+  }
+}
