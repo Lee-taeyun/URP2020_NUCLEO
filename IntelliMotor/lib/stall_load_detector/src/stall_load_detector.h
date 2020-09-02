d230bb39c4c77d7dd1e20b8bfdd75d49c0a1fcab
jaeyeon park <parkjani2@gmail.com>
2020-09-02 20:05:46 +0900

    add regression to IntelliMotor's stall_load_detector

--- IntelliMotor/lib/stall_load_detector/src/stall_load_detector.h
+++ IntelliMotor/lib/stall_load_detector/src/stall_load_detector.h
@@ -10,25 +10,26 @@
 // MAX_STEP_SPEED = NUM_OF_CURRENT_SAMPLE * SKIP_STEP
 #define NUM_OF_CURRENT_SAMPLE   500
 #define MAX_STEP_SPEED          1000 //steps per second
-#define SPEED_HOLDING_TIME          10   //ms
+#define SPEED_HOLDING_TIME      10   //ms
 #define SAMPLE_VALUE_MULTIPLIER 1000
+
+#define MAX_DEPTH_FOR_RECURSION 5
 
 class StallLoadDetector{
     private:
         Ammeter* ammeter;
         AccelStepper* stepper;
-        double currentValues[NUM_OF_CURRENT_SAMPLE];
         const static int skip_step = MAX_STEP_SPEED/NUM_OF_CURRENT_SAMPLE;
         //MAX_STEP_SPEED = NUM_OF_CURRENT_SAMPLE * skip_step
-<<<<<<< HEAD
-        void compressCurrentCharacteristics(double* currentValues);
-=======
-        
->>>>>>> test_branch
     public:
+        Line* lineContainer;
+        int containerLen;
         StallLoadDetector(Ammeter* amm, AccelStepper* stepper);
+        ~StallLoadDetector();
         void measureMotorCharacteristics();
         void measureMotorMeanCharacteristics();
-        double calculateCurrentFromSpeed(int speed);
+        double calculateCurrentFromSpeed(double speed);
+        //now support any speed between 0 - MAX_STEP_SPEED to calculate current value
+        
 };
 #endif
